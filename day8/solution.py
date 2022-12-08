a557daab992583b5d19a53bd0c5ddb5121fc42c1
Alice Lieutier <alice.lieutier@gmail.com>
2022-12-08 10:16:53 +0000

    day8 - part 2

--- day8/solution.py
+++ day8/solution.py
@@ -1,6 +1,7 @@
 #! /opt/homebrew/bin/python3
 import os
 from itertools import repeat
+from functools import reduce
 
 TEST_FILE = f'{os.path.dirname(__file__)}/test_input'
 INPUT_FILE = f'{os.path.dirname(__file__)}/input'
@@ -33,14 +34,36 @@ def hidden_in_direction(forest, tree, direction):
 def is_hidden(forest, tree):
   return all(hidden_in_direction(forest, tree, direction) for direction in DIRECTIONS)
 
+def view_in_direction(forest, tree, direction):
+  x, y = tree
+  tree_height = forest[x][y]
+  view = 0
+  for i,j in trees_in_direction(forest, tree, direction):
+    view += 1
+    if forest[i][j] >= tree_height:
+      break
+  return view
+
+def scenic_score(forest, tree):
+  return reduce(
+    lambda x,y: x*y,
+    (view_in_direction(forest, tree, direction) for direction in DIRECTIONS)
+  )
+
 def process_part_1(forest):
   height, width = len(forest), len(forest[0])
   inner_trees = ((i,j) for i in range(1,height-1) for j in range(1,width-1))
   hidden_trees = [tree for tree in inner_trees if is_hidden(forest, tree)]
   return height*width - len(hidden_trees)
 
+def process_part_2(forest):
+  height, width = len(forest), len(forest[0])
+  trees = ((i,j) for i in range(height) for j in range(width))
+  return max(scenic_score(forest, tree) for tree in trees)
+
 # Solution
 print(process_part_1(parse(INPUT_FILE)))
+print(process_part_2(parse(INPUT_FILE)))
 
 # Tests
 assert is_hidden([[3,3,3],[3,4,3],[3,3,3]], (1,1)) == False
@@ -50,4 +73,16 @@ assert is_hidden([[3,2,3],[3,2,3],[3,1,3]], (1,1)) == False
 assert is_hidden([[5,5,5,5,5],[4,3,4,0,5],[5,5,5,5,5]], (1,2)) == True
 assert is_hidden([[5,5,5,5,5],[4,3,3,0,2],[5,5,5,5,5]], (1,2)) == False
 
-assert process_part_1(parse(TEST_FILE)) == 21
+TEST_FOREST = parse(TEST_FILE)
+assert view_in_direction(TEST_FOREST, (3, 2), 'UP') == 2
+assert view_in_direction(TEST_FOREST, (3, 2), 'LEFT') == 2
+assert view_in_direction(TEST_FOREST, (3, 2), 'DOWN') == 1
+assert view_in_direction(TEST_FOREST, (3, 2), 'RIGHT') == 2
+
+assert view_in_direction(TEST_FOREST, (3, 4), 'UP') == 3
+
+assert scenic_score(TEST_FOREST, (1, 2)) == 4
+assert scenic_score(TEST_FOREST, (3, 2)) == 8
+
+assert process_part_1(TEST_FOREST) == 21
+assert process_part_2(TEST_FOREST) == 8
