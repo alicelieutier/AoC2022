6a009c3694ed1f8d4a32ce8448e0685d7f4e7c38
Alice Lieutier <alice.lieutier@gmail.com>
2022-12-09 00:40:06 +0000

    day8 refactor

--- day8/solution.py
+++ day8/solution.py
@@ -8,31 +8,30 @@ INPUT_FILE = f'{os.path.dirname(__file__)}/input'
 
 def parse(file):
   with open(file) as input:
-    return [list(map(int, line.strip())) for line in input.readlines()]
+    return [[int(x) for x in line.strip()] for line in input.readlines()]
 
 DIRECTIONS = ['UP','DOWN','LEFT','RIGHT']
 
 def trees_in_direction(forest, tree, direction):
   height, width = len(forest), len(forest[0])
   x, y = tree
-  if direction == 'UP':
-    return ((i,j) for i,j in zip(range(x-1,-1,-1), repeat(y)))
-  if direction == 'DOWN':
-    return ((i,j) for i,j in zip(range(x+1,height), repeat(y)))
-  if direction == 'LEFT':
-    return ((i,j) for i,j in zip(repeat(x),range(y-1,-1,-1)))
-  if direction == 'RIGHT':
-    return ((i,j) for i,j in zip(repeat(x),range(y+1, width)))
+  return {
+    'UP': ((i,j) for i,j in zip(range(x-1,-1,-1), repeat(y))),
+    'DOWN': ((i,j) for i,j in zip(range(x+1,height), repeat(y))),
+    'LEFT': ((i,j) for i,j in zip(repeat(x), range(y-1,-1,-1))),
+    'RIGHT': ((i,j) for i,j in zip(repeat(x), range(y+1, width))),
+  }[direction]
 
-def hidden_in_direction(forest, tree, direction):
+def visible_in_direction(forest, tree, direction):
   x, y = tree
   tree_height = forest[x][y]
   for i,j in trees_in_direction(forest, tree, direction):
     if forest[i][j] >= tree_height:
-      return True
+      return False
+  return True
 
-def is_hidden(forest, tree):
-  return all(hidden_in_direction(forest, tree, direction) for direction in DIRECTIONS)
+def is_visible(forest, tree):
+  return any(visible_in_direction(forest, tree, direction) for direction in DIRECTIONS)
 
 def view_in_direction(forest, tree, direction):
   x, y = tree
@@ -52,9 +51,10 @@ def scenic_score(forest, tree):
 
 def process_part_1(forest):
   height, width = len(forest), len(forest[0])
+  nb_of_trees_on_edge = height*2 + width*2 - 4
   inner_trees = ((i,j) for i in range(1,height-1) for j in range(1,width-1))
-  hidden_trees = [tree for tree in inner_trees if is_hidden(forest, tree)]
-  return height*width - len(hidden_trees)
+  visible_inner_trees = [tree for tree in inner_trees if is_visible(forest, tree)]
+  return nb_of_trees_on_edge + len(visible_inner_trees)
 
 def process_part_2(forest):
   height, width = len(forest), len(forest[0])
@@ -66,12 +66,12 @@ print(process_part_1(parse(INPUT_FILE)))
 print(process_part_2(parse(INPUT_FILE)))
 
 # Tests
-assert is_hidden([[3,3,3],[3,4,3],[3,3,3]], (1,1)) == False
-assert is_hidden([[3,3,3],[3,3,3],[3,3,3]], (1,1)) == True
-assert is_hidden([[3,3,3],[1,2,1],[3,3,3]], (1,1)) == False
-assert is_hidden([[3,2,3],[3,2,3],[3,1,3]], (1,1)) == False
-assert is_hidden([[5,5,5,5,5],[4,3,4,0,5],[5,5,5,5,5]], (1,2)) == True
-assert is_hidden([[5,5,5,5,5],[4,3,3,0,2],[5,5,5,5,5]], (1,2)) == False
+assert is_visible([[3,3,3],[3,4,3],[3,3,3]], (1,1)) == True
+assert is_visible([[3,3,3],[3,3,3],[3,3,3]], (1,1)) == False
+assert is_visible([[3,3,3],[1,2,1],[3,3,3]], (1,1)) == True
+assert is_visible([[3,2,3],[3,2,3],[3,1,3]], (1,1)) == True
+assert is_visible([[5,5,5,5,5],[4,3,4,0,5],[5,5,5,5,5]], (1,2)) == False
+assert is_visible([[5,5,5,5,5],[4,3,3,0,2],[5,5,5,5,5]], (1,2)) == True
 
 TEST_FOREST = parse(TEST_FILE)
 assert view_in_direction(TEST_FOREST, (3, 2), 'UP') == 2
