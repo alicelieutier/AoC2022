c17f57d7ec6eb9270cda081edd4fb0962ffa609b
Alice Lieutier <alice.lieutier@gmail.com>
2022-12-16 22:02:53 +0000

    add regex to template

--- template/solution.py
+++ template/solution.py
@@ -1,10 +1,16 @@
 #! /usr/bin/env python3
 import os
+import re
 
 TEST_FILE = f'{os.path.dirname(__file__)}/test_input'
 INPUT_FILE = f'{os.path.dirname(__file__)}/input'
 
+LINE_PATTERN = re.compile(r'^move (?P<nb_of_crates>\d+) from (?P<start>\d+) to (?P<end>\d+)(\s|$)')
 def parse(file):
+  def parse_line(line):
+    match = LINE_PATTERN.search(line)
+    return int(match.group('nb_of_crates'))
+    
   with open(file) as input:
     return [line.strip() for line in input.readlines()]
 
